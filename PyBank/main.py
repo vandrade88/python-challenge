836b0fc7361276b4f8cefc4cd9d4f833f3a41f81
Valerie <vandrade07@yahoo.com>
2021-01-13 19:18:00 -0500

    updated py file, total added

--- PyBank/main.py
+++ PyBank/main.py
@@ -9,26 +9,35 @@ def fin_analysis():
     total_months_amount = 0
     profit_change = 0
     loss_change = 0
+    total_amount = 0
     total_months_list = []
     profit_change_list = []
     loss_change_list = []
+    total_amount_list = []
     
-    # loop through rows to add column 1 into new list
+    # loop through rows to add column A and B into respective new lists
     for row in csv_reader:
         total_months_list.append(row[0])
-        
-    # use len to calculate total months in new list
+        total_amount_list.append(int(row[1]))
+   
+    # use len to calculate total months from list
     total_months_amount = len(total_months_list)
+
+    # calculate sum of all profits/losses from new list
+    total_amount = sum(total_amount_list)
+    
+    # calculate average of values from list
+    #average = sum(total_amount_list) / len(total_amount_list)
         
     # print out the financial analysis
     print("Financial Analysis")
     print("----------------------------------")
     print(f"Total Months: {total_months_amount}")
+    print(f"Total: ${total_amount}")
     #print(f"Average Change: {value}")
     #print(f"Greatest Increase in Profits: {value}")
     #print(f"Greatest Decrease in Profits: {value}")
     
-    # need return because function parameter was empty
     return
 
 # open and read csv file
